64afde64e3d9e79fe8bfff1dc2466a636a102bbb
Benedikt Werner <1benediktwerner@gmail.com>
2022-11-10 05:33:49 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{
     error::Error,
     io::{BufRead, BufReader, Write},
+    process::{Command, Stdio},
 };
 
 #[allow(dead_code)]
@@ -44,74 +45,68 @@ struct Engine {
 fn main() -> Result<(), Box<dyn Error>> {
     // Step 1) Long poll for analysis requests
     // When a move is made on the Analysis board, it will be returned from this endpoint
-    let analysis_request =
-        reqwest::blocking::get("http://localhost:3000/api/external-engine/work")?
-            .json::<AnalysisRequest>()?;
+    let analysis_request = ureq::get("http://localhost:3000/api/external-engine/work")
+        .call()?
+        .into_json::<AnalysisRequest>()?;
 
-    println!("{:#?}", analysis_request);
+    println!("{analysis_request:#?}");
 
     // Step 2) Start a POST request stream to /api/external-engine/work/{id}
     // http://localhost:3000/api/external-engine/work/{}
     // todo
 
     // Step 3) Send the FEN to the engine
-    let mut engine = std::process::Command::new("./stockfish")
-        .stdin(std::process::Stdio::piped())
-        .stdout(std::process::Stdio::piped())
+    let mut engine = Command::new("./stockfish")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
         .spawn()?;
 
     let engine_stdin = engine.stdin.as_mut().ok_or("Failed to get stdin")?;
 
     // Set UCI options
-    engine_stdin.write_all(
-        format!(
-            "setoption name Threads value {}\n",
-            analysis_request.work.threads
-        )
-        .as_bytes(),
+    writeln!(
+        engine_stdin,
+        "setoption name Threads value {}",
+        analysis_request.work.threads
     )?;
-    engine_stdin.write_all(
-        format!("setoption name Hash value {}\n", analysis_request.work.hash).as_bytes(),
+    writeln!(
+        engine_stdin,
+        "setoption name Hash value {}",
+        analysis_request.work.hash
     )?;
-    engine_stdin.write_all(
-        format!(
-            "setoption name MultiPV value {}\n",
-            analysis_request.work.multi_pv
-        )
-        .as_bytes(),
+    writeln!(
+        engine_stdin,
+        "setoption name MultiPV value {}",
+        analysis_request.work.multi_pv
     )?;
-    engine_stdin.write_all(
-        format!(
-            "setoption name Variant value {}\n",
-            analysis_request.work.variant
-        )
-        .as_bytes(),
+    writeln!(
+        engine_stdin,
+        "setoption name Variant value {}",
+        analysis_request.work.variant
     )?;
-
-    let _ = engine_stdin.write_all(
-        format!(
-            "position fen {} moves {}\n",
-            analysis_request.work.initial_fen,
-            analysis_request.work.moves.join(" ")
-        )
-        .as_bytes(),
-    );
+    writeln!(
+        engine_stdin,
+        "position fen {} moves {}",
+        analysis_request.work.initial_fen,
+        analysis_request.work.moves.join(" ")
+    )?;
 
     if analysis_request.work.infinite {
-        let _ = engine_stdin.write_all(b"go infinite\n");
+        writeln!(engine_stdin, "go infinite")?;
     } else {
-        let _ = engine_stdin
-            .write_all(format!("go depth {}\n", analysis_request.engine.default_depth).as_bytes());
+        writeln!(
+            engine_stdin,
+            "go depth {}\n",
+            analysis_request.engine.default_depth
+        )?;
     }
 
     engine_stdin.flush()?;
 
     let engine_stdout = engine.stdout.as_mut().ok_or("Failed to get stdout")?;
 
-    let mut reader = BufReader::new(engine_stdout);
-    loop {
-        let mut line = String::new();
-        let _ = reader.read_line(&mut line);
+    for line in BufReader::new(engine_stdout).lines() {
+        let line = line?;
         println!("Engine: {}", line.trim());
         if line.contains("info") {
             // Step 4) Send the "info" line to the server
